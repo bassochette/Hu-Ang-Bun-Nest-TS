2ec372ad22d67fdba7dbdaa437e1a5c60c23185f
Julien Prugne <julien@webeleon.dev>
2025-03-19 00:14:49 +0100

    perf(api): enable compression

--- api/bootstrap.ts
+++ api/bootstrap.ts
@@ -1,13 +1,14 @@
 import { Logger } from '@nestjs/common';
 import { NestApplication, NestFactory } from '@nestjs/core';
+import compression from 'compression';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const app = await NestFactory.create<NestApplication>(AppModule);
 
 app.enableCors();
-
 app.use(helmet());
+app.use(compression())
 
 const port = process.env.PORT || 3000;
 await app.listen(port);
