b8e95e0a70a50c08339730413f82a4b6a99eaab5
Julien Prugne <julien@webeleon.dev>
2025-03-19 00:08:27 +0100

    security(api): add helmet

--- api/bootstrap.ts
+++ api/bootstrap.ts
@@ -1,10 +1,13 @@
 import { Logger } from '@nestjs/common';
 import { NestApplication, NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const app = await NestFactory.create<NestApplication>(AppModule);
 
 app.enableCors();
+
+app.use(helmet());
 
 const port = process.env.PORT || 3000;
 await app.listen(port);
