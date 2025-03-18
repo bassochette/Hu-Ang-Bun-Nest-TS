6aa4379721a2b83b0447b5b9da0d1a0be3983cea
Julien Prugne <julien@webeleon.dev>
2025-03-19 00:32:56 +0100

    feat(api): swagger documentation

--- api/bootstrap.ts
+++ api/bootstrap.ts
@@ -1,16 +1,29 @@
 import { Logger } from '@nestjs/common';
 import { NestApplication, NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import compression from 'compression';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 const app = await NestFactory.create<NestApplication>(AppModule);
 
+/* Middleware */
 app.enableCors();
 app.use(helmet());
 app.use(compression())
 
+/* Open API */
+const openAPIConfig = new DocumentBuilder()
+    .setTitle('🦊 API')
+    .setDescription('The 🦊 API description')
+    .addServer('http://localhost:3000')
+    .setVersion('1.0')
+    .build()
+
+const document = SwaggerModule.createDocument(app, openAPIConfig);
+SwaggerModule.setup('swagger', app, document);
+  
+/* Start */
 const port = process.env.PORT || 3000;
 await app.listen(port);
-
 Logger.log(`🦊 API：started on port ${port} 🦊`);
